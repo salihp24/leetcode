b1efac1daa170fdba94a4049b0bc045b42bc7243
salihp24 <salihp2423@gmail.com>
2025-12-19 09:35:34 +0530

    Time: 11 ms (26.78%), Space: 65.5 MB (24.27%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let str=x.toString()
+    let rev=str.split("").reverse().join("")
+
+    return str===rev;
+};
