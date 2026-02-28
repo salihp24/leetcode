ef06b01447f20d3e112518a1d154d18e0bfc74f1
salihp24 <salihp2423@gmail.com>
2026-02-28 10:42:41 +0530

    Time: 52 ms (46.02%), Space: 57.9 MB (17.02%) - LeetHub

--- 2665-counter-ii/2665-counter-ii.js
+++ 2665-counter-ii/2665-counter-ii.js
@@ -1,7 +1,4 @@
-/**
- * @param {integer} init
- * @return { increment: Function, decrement: Function, reset: Function }
- */
+
 var createCounter = function(init) {
     let count=init
     return{
@@ -10,10 +7,3 @@ var createCounter = function(init) {
         reset:()=>count=init
     }
 };
-
-/**
- * const counter = createCounter(5)
- * counter.increment(); // 6
- * counter.reset(); // 5
- * counter.decrement(); // 4
- */
