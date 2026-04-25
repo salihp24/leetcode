67dfedcb62f5a0f94eb343d3f28272b6881c13b3
salihp24 <salihp2423@gmail.com>
2026-04-25 10:30:35 +0530

    Time: 29 ms (37.32%), Space: 54 MB (89.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,14 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
 var twoSum = function(nums, target) {
-for(i=0; i<=nums.length; i++){
-    for(j=i+1; j<nums.length;j++){
-      if( nums[i]+nums[j] === target) {
-          return [i,j]
-      }
-
+    for(let i=0; i<nums.length; i++){
+        for(let a=i+1; a<nums.length; a++){
+            if(nums[i]+nums[a]==target){
+                return [i,a]
+            }
+        }
     }
-     
-}
- 
-    
-    
 };
