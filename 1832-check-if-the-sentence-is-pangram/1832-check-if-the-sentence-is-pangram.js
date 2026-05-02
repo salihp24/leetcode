49878199e71045e61b9c897f9f39c88ac6916cf8
salihp24 <salihp2423@gmail.com>
2026-05-02 11:43:00 +0530

    Time: 4 ms (16.64%), Space: 53.2 MB (93.67%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} sentence
+ * @return {boolean}
+ */
+var checkIfPangram = function(sentence) {
+    let value=[]
+
+    for (let i=0; i<sentence.length;i++){
+        if(!value.includes(sentence[i])){
+            value+=sentence[i]
+        }
+    }
+
+    if(value.length==26){
+        return true
+    }else{
+        return false
+    }
+
+};
