3b776895b08e23956a592760309665d780a45f88
salihp24 <salihp2423@gmail.com>
2026-05-09 12:17:02 +0530

    Time: 0 ms (100%), Space: 57.4 MB (17.44%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canAliceWin = function(nums) {
+    let single=0
+    let double=0
+
+    for(let i=0; i<nums.length;i++){
+        if(nums[i]>=10){
+            double+=nums[i]
+        }else{
+            single+=nums[i]
+        }
+    }
+
+    return single!==double
+    
+};
