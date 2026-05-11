842197e9d90404ea8d625fd7a746926a97dd0c36
salihp24 <salihp2423@gmail.com>
2026-05-11 10:00:48 +0530

    Time: 25 ms (5.74%), Space: 61 MB (5.07%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.js
+++ 2652-sum-multiples/2652-sum-multiples.js
@@ -2,13 +2,19 @@
  * @param {number} n
  * @return {number}
  */
-var sumOfMultiples = function(n) {
-    let result=0
-    for (let i=1; i<=n; i++){
-        if(i%3==0 || i%5==0 || i%7==0){
-            result+=i
-        }
-    }
+// var sumOfMultiples = function(n) {
+//     let result=0
+//     for (let i=1; i<=n; i++){
+//         if(i%3==0 || i%5==0 || i%7==0){
+//             result+=i
+//         }
+//     }
 
-    return result
+//     return result
+// };
+
+var sumOfMultiples = function(n) {
+    return Array.from({length:n},(_,i)=>i+1)
+    .filter(v=>v%3==0 || v%5==0 || v%7==0)
+    .reduce((acc,val)=>acc+val,0)
 };
