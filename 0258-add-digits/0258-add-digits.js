6a2fee076014d5e6c9f231f4ed8f649dd7da9693
salihp24 <salihp2423@gmail.com>
2026-05-18 18:54:41 +0530

    Time: 1 ms (55.74%), Space: 55.7 MB (79.93%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+    if(num==0){
+        return 0
+    }
+    
+    while(num>=10){
+        let sum=0
+        while(num>0){
+            sum+=num%10
+            num=Math.floor(num/10)
+        }
+        num=sum
+        
+    }
+    return num
+};
